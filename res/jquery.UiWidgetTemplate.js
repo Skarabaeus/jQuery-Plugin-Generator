6e9e2179f16e5f8a5f65232fe0a28e7dc8521df0
Stefan Siebel <stefan@Stefan-Siebels-iMac.local>
2011-02-26 17:21:56 +0100

    don't use closure for private functions. Less effective for minification, but better for performance.

--- res/jquery.UiWidgetTemplate.js
+++ res/jquery.UiWidgetTemplate.js
@@ -1,30 +1,36 @@
-// for more information about developing widgets with jQuery UI
-// checkout http://jqueryui.com/docs/Developer_Guide
-
 (function(){
 	var #{plugin_name} = function(){
 
-		var _myPrivateFunction = function() {
-		};
+		/**
+		* Private functions
+		* functions starting with a _ are pseudo private.
+		* They cannot be access from the outside.
+		*/
+		_myPrivateFunction: function() {
 
-		// put all private functions in here
-		// that improves minification. See http://blog.project-sierra.de/archives/1622
+		},
 
-		return {
-			// default options
-			options: {
+		// required function. Automatically called when widget is created
+		_create: function() {
+			// you can access the currently select DOM element through
+			// this.element
+		},
 
-			},
+		// Once the element is created, all other calls to the widget
+		// name where the first parameter is not a string will call the _init()
+		// method; if options are passed, the .option() method will be called
+		// before the _init() method.
+		_init: function() {
 
-			// required function. Automatically called when widget is created
-			_create: function() {
-				// you can access the currently select DOM element through
-				// this.element
-			},
+		},
 
-			destroy: function() {
+		options: {
+		},
 
-			}
+
+		destroy: function() {
+
+		}
 		};
 	}();
 	// register widget
